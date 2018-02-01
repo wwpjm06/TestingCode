1930f4c354dca674e4c1b795c20b3f056c2b8123
wwpjm06 <34771175+wwpjm06@users.noreply.github.com>
2018-02-01 18:53:48 +0000

    Update aTemplate.py

--- aTemplate.py
+++ aTemplate.py
@@ -14,4 +14,4 @@ while sCarryOn == "C":
     
     sCarryOn=input("\nPress c <<then Enter>> to Continue or any other key <<then Enter>> to Exit >>>>>>\n\n").upper()
 #End of CarryOn While
-print("Program terminated as you pressed \"" + sCarryOn +"\"")
+print("Program terminated as you pressed the key\"" + sCarryOn +"\"")
