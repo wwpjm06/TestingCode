f874f0c5708ce20b7f8d4aeba6c24ca10de1525c
wwpjm06 <34771175+wwpjm06@users.noreply.github.com>
2018-02-06 16:22:17 +0000

    Just started

---
+++ aTestRelay.py
@@ -0,0 +1,45 @@
+# Testing program for JQC-3FF-=S-Z relay 
+
+import RPi.GPIO as GPIO
+#import time
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+
+#sCarryOn="C"
+#while sCarryOn == "C":
+
+try:
+    # here you put your main loop or block of code  
+    # loop through 50 times, on/off for 1 second
+    #for i in range(10):
+    print("Before set Pin")
+    GPIO.output(17,True)
+    print("After set Pin")
+    #time.sleep(15)
+    sleep(5)
+    print("After Sleep time")
+     #       GPIO.output(17,False)
+     #       time.sleep(1)
+  
+except KeyboardInterrupt:  
+    # here you put any code you want to run before the program
+    print("Keyboard Interrupt")
+     # exits when you press CTRL+C  
+except:  
+    # this catches ALL other exceptions including errors.  
+    # You won't get any error messages for debugging  
+    # so only use it once your code is working  
+    print("Other error or exception occurred!")  
+  
+finally:  
+   GPIO.cleanup() # this ensures a clean exit
+print(" - after Cleanup")
+
+
+
+    
+    #sCarryOn=input("\nPress c <<then Enter>> to Continue or any other key <<then Enter>> to Exit >>>>>>\n\n").upper()
+#End of CarryOn While
+#print("Program terminated as you pressed \"" + sCarryOn +"\"")
